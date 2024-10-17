7b7f5dd9dd2d28ca7f390f651b70870d3230b122
Raven <ravenn@tuta.io>
2024-10-17 10:23:38 -0400

    Moved components and added file selector functionality

--- GUI/Main.Designer.cs
+++ GUI/Main.Designer.cs
@@ -61,42 +61,47 @@ namespace GUI
         {
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
-            panel1 = new Panel();
-            settings_button = new RoundedButton();
+            selectorPanel = new Panel();
+            report_button = new RoundedButton();
             selector_panel_buttons = new ImageList(components);
+            settings_button = new RoundedButton();
             redaction_options_button = new RoundedButton();
             options_panel_tooltip = new ToolTip(components);
-            report_button = new RoundedButton();
-            panel1.SuspendLayout();
+            splitContainer1 = new SplitContainer();
+            fileSelectorLabel = new Label();
+            fileListView = new ListView();
+            filePathHeader = new ColumnHeader();
+            fileSizeHeader = new ColumnHeader();
+            fileExtensionHeader = new ColumnHeader();
+            selectorPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
             // 
-            // panel1
+            // selectorPanel
             // 
-            panel1.BackColor = Color.FromArgb(43, 45, 48);
-            panel1.Controls.Add(report_button);
-            panel1.Controls.Add(settings_button);
-            panel1.Controls.Add(redaction_options_button);
-            panel1.Dock = DockStyle.Left;
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(77, 732);
-            panel1.TabIndex = 0;
+            selectorPanel.BackColor = Color.FromArgb(43, 45, 48);
+            selectorPanel.Controls.Add(report_button);
+            selectorPanel.Controls.Add(settings_button);
+            selectorPanel.Controls.Add(redaction_options_button);
+            selectorPanel.Dock = DockStyle.Left;
+            selectorPanel.Location = new Point(0, 0);
+            selectorPanel.Name = "selectorPanel";
+            selectorPanel.Size = new Size(77, 732);
+            selectorPanel.TabIndex = 0;
             // 
-            // settings_button
+            // report_button
             // 
-            settings_button.FlatAppearance.BorderColor = Color.FromArgb(43, 45, 48);
-            settings_button.FlatStyle = FlatStyle.Flat;
-            settings_button.ImageIndex = 1;
-            settings_button.ImageList = selector_panel_buttons;
-            settings_button.Location = new Point(12, 66);
-            settings_button.Name = "settings_button";
-            settings_button.Size = new Size(48, 48);
-            settings_button.TabIndex = 2;
-            options_panel_tooltip.SetToolTip(settings_button, "Display the settings");
-            settings_button.UseVisualStyleBackColor = true;
-            settings_button.Click += settings_button_Click;
-            settings_button.MouseEnter += settings_button_MouseEnter;
-            settings_button.MouseLeave += settings_button_MouseLeave;
+            report_button.FlatStyle = FlatStyle.Flat;
+            report_button.ImageIndex = 2;
+            report_button.ImageList = selector_panel_buttons;
+            report_button.Location = new Point(12, 12);
+            report_button.Name = "report_button";
+            report_button.Size = new Size(48, 48);
+            report_button.TabIndex = 3;
+            options_panel_tooltip.SetToolTip(report_button, "Show reports page");
+            report_button.UseVisualStyleBackColor = true;
             // 
             // selector_panel_buttons
             // 
@@ -107,13 +112,29 @@ namespace GUI
             selector_panel_buttons.Images.SetKeyName(1, "icons8-settings-48.png");
             selector_panel_buttons.Images.SetKeyName(2, "icons8-report-48.png");
             // 
+            // settings_button
+            // 
+            settings_button.FlatAppearance.BorderColor = Color.FromArgb(43, 45, 48);
+            settings_button.FlatStyle = FlatStyle.Flat;
+            settings_button.ImageIndex = 1;
+            settings_button.ImageList = selector_panel_buttons;
+            settings_button.Location = new Point(12, 618);
+            settings_button.Name = "settings_button";
+            settings_button.Size = new Size(48, 48);
+            settings_button.TabIndex = 2;
+            options_panel_tooltip.SetToolTip(settings_button, "Display the settings");
+            settings_button.UseVisualStyleBackColor = true;
+            settings_button.Click += settings_button_Click;
+            settings_button.MouseEnter += settings_button_MouseEnter;
+            settings_button.MouseLeave += settings_button_MouseLeave;
+            // 
             // redaction_options_button
             // 
             redaction_options_button.FlatAppearance.BorderColor = Color.FromArgb(43, 45, 48);
             redaction_options_button.FlatStyle = FlatStyle.Flat;
             redaction_options_button.ImageIndex = 0;
             redaction_options_button.ImageList = selector_panel_buttons;
-            redaction_options_button.Location = new Point(12, 12);
+            redaction_options_button.Location = new Point(12, 672);
             redaction_options_button.Name = "redaction_options_button";
             redaction_options_button.Size = new Size(48, 48);
             redaction_options_button.TabIndex = 1;
@@ -127,28 +148,72 @@ namespace GUI
             // 
             options_panel_tooltip.ToolTipTitle = "Options Panel";
             // 
-            // report_button
+            // splitContainer1
             // 
-            report_button.FlatStyle = FlatStyle.Flat;
-            report_button.ImageIndex = 2;
-            report_button.ImageList = selector_panel_buttons;
-            report_button.Location = new Point(12, 672);
-            report_button.Name = "report_button";
-            report_button.Size = new Size(48, 48);
-            report_button.TabIndex = 3;
-            options_panel_tooltip.SetToolTip(report_button, "Show reports page");
-            report_button.UseVisualStyleBackColor = true;
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(77, 0);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(fileSelectorLabel);
+            splitContainer1.Panel1.Controls.Add(fileListView);
+            splitContainer1.Size = new Size(1192, 732);
+            splitContainer1.SplitterDistance = 397;
+            splitContainer1.TabIndex = 1;
+            // 
+            // fileSelectorLabel
+            // 
+            fileSelectorLabel.Dock = DockStyle.Fill;
+            fileSelectorLabel.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            fileSelectorLabel.Location = new Point(0, 0);
+            fileSelectorLabel.Name = "fileSelectorLabel";
+            fileSelectorLabel.Size = new Size(1192, 397);
+            fileSelectorLabel.TabIndex = 0;
+            fileSelectorLabel.Text = "Click to Select or Drag and Drop Files";
+            fileSelectorLabel.TextAlign = ContentAlignment.MiddleCenter;
+            fileSelectorLabel.Click += fileSelectorLabel_Click;
+            // 
+            // fileListView
+            // 
+            fileListView.Columns.AddRange(new ColumnHeader[] { filePathHeader, fileSizeHeader, fileExtensionHeader });
+            fileListView.Dock = DockStyle.Fill;
+            fileListView.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+            fileListView.Location = new Point(0, 0);
+            fileListView.Name = "fileListView";
+            fileListView.Size = new Size(1192, 397);
+            fileListView.TabIndex = 1;
+            fileListView.UseCompatibleStateImageBehavior = false;
+            fileListView.View = View.Details;
+            fileListView.Visible = false;
+            // 
+            // filePathHeader
+            // 
+            filePathHeader.Text = "File Path";
+            // 
+            // fileSizeHeader
+            // 
+            fileSizeHeader.Text = "File Size";
+            // 
+            // fileExtensionHeader
+            // 
+            fileExtensionHeader.Text = "File Extension";
             // 
             // Main
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1269, 732);
-            Controls.Add(panel1);
+            Controls.Add(splitContainer1);
+            Controls.Add(selectorPanel);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Main";
             Text = "Data Redaction Toolkit";
-            panel1.ResumeLayout(false);
+            selectorPanel.ResumeLayout(false);
+            splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -164,11 +229,17 @@ namespace GUI
 
         #endregion
 
-        private Panel panel1;
+        private Panel selectorPanel;
         private ImageList selector_panel_buttons;
-        private Button redaction_options_button;
         private ToolTip options_panel_tooltip;
-        private Button settings_button;
-        private Button report_button;
+        private SplitContainer splitContainer1;
+        private Label fileSelectorLabel;
+        private RoundedButton redaction_options_button;
+        private RoundedButton settings_button;
+        private RoundedButton report_button;
+        private ListView fileListView;
+        private ColumnHeader filePathHeader;
+        private ColumnHeader fileSizeHeader;
+        private ColumnHeader fileExtensionHeader;
     }
 }
--- GUI/Main.cs
+++ GUI/Main.cs
@@ -2,7 +2,7 @@ namespace GUI
 {
     public partial class Main : Form
     {
-        private Button selectedButton = null;
+        private Button _selectedButton = null;
 
         public Main()
         {
@@ -12,18 +12,18 @@ namespace GUI
         // Method to change the color of selected button
         private void SelectButton(Button button)
         {
-            if (selectedButton != null)
+            if (_selectedButton != null)
             {
                 // Reset the previous selected button back to it's default color
-                selectedButton.BackColor = Color.Transparent;
+                _selectedButton.BackColor = Color.Transparent;
             }
 
             // Set the new selected button
-            selectedButton = button;
-            if (selectedButton != null)
+            _selectedButton = button;
+            if (_selectedButton != null)
             {
                 // Change the color of the selected button
-                selectedButton.BackColor = System.Drawing.ColorTranslator.FromHtml("#4b4c4f");
+                _selectedButton.BackColor = System.Drawing.ColorTranslator.FromHtml("#4b4c4f");
             }
         }
 
@@ -34,7 +34,7 @@ namespace GUI
 
         private void redaction_options_button_MouseEnter(object sender, EventArgs e)
         {
-            if (selectedButton != redaction_options_button)
+            if (_selectedButton != redaction_options_button)
             {
                 redaction_options_button.BackColor = System.Drawing.ColorTranslator.FromHtml("#4b4c4f");
             }
@@ -42,7 +42,7 @@ namespace GUI
 
         private void redaction_options_button_MouseLeave(object sender, EventArgs e)
         {
-            if (selectedButton != redaction_options_button)
+            if (_selectedButton != redaction_options_button)
             {
                 redaction_options_button.BackColor = Color.Transparent;
             }
@@ -55,7 +55,7 @@ namespace GUI
 
         private void settings_button_MouseEnter(object sender, EventArgs e)
         {
-            if (selectedButton != settings_button)
+            if (_selectedButton != settings_button)
             {
                 settings_button.BackColor = System.Drawing.ColorTranslator.FromHtml("#4b4c4f");
             }
@@ -63,9 +63,36 @@ namespace GUI
 
         private void settings_button_MouseLeave(object sender, EventArgs e)
         {
-            if (selectedButton != settings_button)
+            if (_selectedButton != settings_button)
             {
                 settings_button.BackColor = Color.Transparent;
+            }
+        }
+
+        private void fileSelectorLabel_Click(object sender, EventArgs e)
+        {
+            using var openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true; // Allow selection of multiple files
+
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            // Hide the drag and drop label, then show the fileListView
+            fileSelectorLabel.Visible = false;
+            fileListView.Visible = true;
+
+            // Add selected files to list view
+            var files = openFileDialog.FileNames;
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                var item = new ListViewItem(new[]
+                {
+                    fileInfo.Name,
+                    fileInfo.Length.ToString(),
+                    fileInfo.Extension
+                });
+                fileListView.Items.Add(item);
             }
         }
     }
