8cba92eec28fa1fe60b641ec4ddb37dd4c038af8
Raven <ravenn@tuta.io>
2024-10-17 04:19:53 -0400

    Implemented TextFileRedactor

--- Backend/src/main/java/com/ravesec/drtk/strategy/TextFileRedactor.java
+++ Backend/src/main/java/com/ravesec/drtk/strategy/TextFileRedactor.java
@@ -2,9 +2,16 @@ package com.ravesec.drtk.strategy;
 
 import com.ravesec.drtk.core.RedactionLevel;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class TextFileRedactor implements RedactionStrategy {
     @Override
     public void redact(String filePath, String[] keywords, RedactionLevel redactionLevel) throws Exception {
-
+        Path path = Paths.get(filePath);
+        String content = Files.readString(path);
+        content = redactionLevel.applyRedaction(content, keywords);
+        Files.writeString(path, content);
     }
 }
