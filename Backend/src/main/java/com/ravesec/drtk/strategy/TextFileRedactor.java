857ccc04d86384456e9893d8a93de67a8f91f255
Raven <ravenn@tuta.io>
2024-10-17 06:12:32 -0400

    Modified the text redactor to handle different encodings

--- Backend/src/main/java/com/ravesec/drtk/strategy/TextFileRedactor.java
+++ Backend/src/main/java/com/ravesec/drtk/strategy/TextFileRedactor.java
@@ -2,6 +2,11 @@ package com.ravesec.drtk.strategy;
 
 import com.ravesec.drtk.core.RedactionLevel;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.MalformedInputException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,9 +14,44 @@ import java.nio.file.Paths;
 public class TextFileRedactor implements RedactionStrategy {
     @Override
     public void redact(String filePath, String[] keywords, RedactionLevel redactionLevel) throws Exception {
+//        Path path = Paths.get(filePath);
+//        String content = Files.readString(path, StandardCharsets.UTF_8);
+//        content = redactionLevel.applyRedaction(content, keywords);
+//        Files.writeString(path, content, StandardCharsets.UTF_8);
         Path path = Paths.get(filePath);
-        String content = Files.readString(path);
+        String content = null;
+        Charset correctCharset = null;
+
+        Charset[] charsets = new Charset[]{
+                StandardCharsets.UTF_8,
+                StandardCharsets.ISO_8859_1,
+                StandardCharsets.US_ASCII,
+                StandardCharsets.UTF_16BE,
+                StandardCharsets.UTF_16,
+                StandardCharsets.UTF_16LE,
+                StandardCharsets.UTF_32,
+                StandardCharsets.UTF_32BE,
+                StandardCharsets.UTF_32LE
+        };
+
+        for (Charset charset : charsets) {
+            try {
+                content = Files.readString(path, charset);
+                correctCharset = charset;
+                break;
+            } catch (MalformedInputException _) {
+                ;
+            }
+        }
+
+        if (content == null) {
+            throw new IOException("Unable to redact text file: " + filePath);
+        }
+
         content = redactionLevel.applyRedaction(content, keywords);
-        Files.writeString(path, content);
+
+        try (BufferedWriter writer = Files.newBufferedWriter(path, correctCharset)) {
+            writer.write(content);
+        }
     }
 }
