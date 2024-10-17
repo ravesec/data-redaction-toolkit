0784d02f587488db078f1349be7dd417dd27a008
Raven <ravenn@tuta.io>
2024-10-17 04:15:39 -0400

    Implemented FileRedactor concrete class

--- Backend/src/main/java/com/ravesec/drtk/redactor/FileRedactor.java
+++ Backend/src/main/java/com/ravesec/drtk/redactor/FileRedactor.java
@@ -1,16 +1,17 @@
 package com.ravesec.drtk.redactor;
 
 import com.ravesec.drtk.core.RedactionLevel;
+import com.ravesec.drtk.factory.RedactionStrategyFactory;
 import com.ravesec.drtk.strategy.RedactionStrategy;
 
 public class FileRedactor {
     private RedactionStrategy strategy;
 
     public FileRedactor(String filePath) {
-
+        this.strategy = RedactionStrategyFactory.getRedactionStrategy(filePath);
     }
 
     public void redactFile(String filePath, String[] sensitiveData, RedactionLevel level) throws Exception {
-
+        strategy.redact(filePath, sensitiveData, level);
     }
 }
