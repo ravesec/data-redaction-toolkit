a9bbacb4dd82a0062976d109fd686bbfd83b58c3
Raven <ravenn@tuta.io>
2024-10-22 10:34:48 -0400

    Added keyword entry functionality

--- drtk-gui/drtk-gui/Views/MainWindow.axaml.cs
+++ drtk-gui/drtk-gui/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace drtk_gui.Views;
 
@@ -7,5 +9,26 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void InputElement_OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Space || e.Key == Key.Enter)
+        {
+            var textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                var text = textBox.Text.TrimEnd();
+                var words = text.Split(' ');
+
+                if (words.Length > 0)
+                {
+                    string lastWord = words[^1];
+                    Console.WriteLine(lastWord); // Print the last word
+                    textBox.Text = string.Join(' ', words[..^1]); // Remove the last word
+                }
+            }
+            e.Handled = true; // Mark the event as handled
+        }
     }
 }
