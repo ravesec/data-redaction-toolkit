3f5b32285eb1bd6ba53356e44b4f2871deb1d0f0
Raven <ravenn@tuta.io>
2024-10-22 10:04:47 -0400

    Basic GUI layout

--- drtk-gui/drtk-gui/ViewModels/MainWindowViewModel.cs
+++ drtk-gui/drtk-gui/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,5 @@
 public partial class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
 }
